37892b38198f7064282d5e8fa879da48ff86bd4b
SupritD <supritdagade77@gmail.com>
2025-07-21 18:02:34 +0530

    auth added to project

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -13,3 +14,16 @@ Route::get('/staff', [MainController::class, 'staff'])->name('staff');
 Route::get('/blog', [MainController::class, 'blog'])->name('blog');
 Route::get('/appointment', [MainController::class, 'appointment'])->name('appointment');
 Route::get('/contact', [MainController::class, 'contact'])->name('contact');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
