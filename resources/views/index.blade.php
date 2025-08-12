54b13ea1539b4c8bc1bbaeee4c8b8c17f7b22955
Mahfooz-Siddiqui <mahfoozsiddiqui490@gmail.com>
2025-08-12 13:42:32 +0530

    home page last two servies image added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -318,7 +318,7 @@
                     <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                         <div class="service-box hover-wrap">
                             <div class="hover-img">
-                                <img src="{{ asset('assets/images/help/services/Endoscopic-Skull-Base-Surgery.png') }}"
+                                <img src="{{ asset('assets/images/help/services/brain-surgery.jpeg') }}"
                                     alt="Service Images" />
                                 <div class="service-detail hover-info">
                                     <a href="{{ route('endoscopic-skull-base-surgery') }}" class="button primary">Know
@@ -339,7 +339,7 @@
                     <div class="col-lg-4 col-md-6 col-sm-12 p-3">
                         <div class="service-box hover-wrap">
                             <div class="hover-img">
-                                <img src="{{ asset('assets/images/help/services/Endoscopic-Skull-Base-Surgery.png') }}"
+                                <img src="{{ asset('assets/images/help/services/spine-surgery.jpeg') }}"
                                     alt="Service Images" />
                                 <div class="service-detail hover-info">
                                     <a href="{{ route('spine-surgery') }}" class="button primary">Know
